ca889aeb9d725871d8222472a26a3ffd9d488bd5
Kels <104536953+xkellss@users.noreply.github.com>
2023-02-09 20:30:56 -0600

    Add files via upload

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,29 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import {useNavigate} from "react-router-dom";
+
+function NewMeetupsPage() {
+    const navigate = useNavigate();
+
+    function addMeetupHandler(meetupData) {
+        fetch('https://react-project-7ba08-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            ).then(() => {
+            navigate('/');
+        });
+    }
+
+    return (
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+        </section>
+    );
+}
+
+export default NewMeetupsPage;
